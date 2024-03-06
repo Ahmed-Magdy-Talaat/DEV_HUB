7fd2a252becf34f3b9560e672f3524aa0a75070a
Ahmed-Magdy-Talaat <Ahmed20227@feng.bu.edu.eg>
2024-03-06 17:25:06 +0200

    implementing Auth using clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="h1-bold">Hello nextjs</h1>
-      <h1 className="h2-bold">Hello nextjs</h1>
-      <h1 className="h3-bold">Hello nextjs</h1>
+    <div className="h-screen">
+      <UserButton />
     </div>
   );
 }
